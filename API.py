f12077d69034a9e8f008ea6a0132412cb40f005c
leahpattison <leah.pattison@hotmail.co.uk>
2018-12-24 15:43:42 +0000

    API weather & spotify
    Spotify & weather API converted to functions. Appending to spreadsheets put into try except blocks - no data added to spreadsheets when spotify track not playing.

---
+++ API.py
@@ -0,0 +1,47 @@
+import json
+import spotipy
+import spotipy.util as util
+from requests import get
+from datetime import datetime
+
+
+def Spotify_API(your_username, scope, client_id, client_secret, redirect_uri):
+    token = util.prompt_for_user_token(
+        username=your_username,
+        scope=scope,
+        client_id=client_id,
+        client_secret=client_secret,
+        redirect_uri=redirect_uri)
+
+    # Create spotify object with permissions
+    spotifyObject = spotipy.Spotify(auth=token)
+
+    # Get current playing track
+    devices = spotifyObject.current_user_playing_track()
+
+    # Connvert UNIX time stamp to YMDHMS
+    time = devices['timestamp']/1E3
+    timedate = datetime.fromtimestamp(int(time)).strftime('%Y-%m-%d %H:%M:%S')
+
+    name = devices['item']['name'] #song name
+    url = devices['item']['external_urls']['spotify'] # song url
+    artist = devices['item']['album']['artists'][0]['name'] #artist name
+    album = devices['item']['album']['name'] #album name
+    duration = devices['item']['duration_ms'] #song duration
+    album_image_url = devices['item']['album']['images'][0]['url'] #album image url
+    trackplaying = devices['is_playing'] #T/F track is playing
+    trackid = devices['item']['uri'] #Spotify track ID
+
+    return [timedate, name, artist, album, duration,album_image_url, url, trackid, trackplaying]
+
+def dark_sky_API(key):
+    sheffyT = ("51.505", "-0.196") #Latitude and longitude for sheffield terrace, london
+    Dark = "https://api.darksky.net/forecast/{}/{loc[0]:},{loc[1]:}?".format(key, loc=sheffyT)
+    weather = get(Dark)
+    currentW = json.loads(weather.text)['currently'] #Current weather data
+
+    #Connvert UNIX time stamp to YMDHMS
+    currentTime = currentW['time']
+    timedate = datetime.fromtimestamp(int(currentTime)).strftime('%Y-%m-%d %H:%M:%S')
+
+    return [timedate, currentW['cloudCover'], currentW['windSpeed'], currentW['temperature'], currentW['humidity'], currentW['icon'], currentW['precipIntensity']]
--- Main.py
+++ Main.py
@@ -1,42 +1,52 @@
-import os
-import sys
-import json
-import spotipy
-import webbrowser
-import spotipy.util as util
-from json.decoder import JSONDecodeError
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from API import Spotify_API, dark_sky_API
+##### SENSING-IOT COURSEWORK 2018 #####
+# Script pulls current playing track data from users spotify & weather data from London, appends to googlesheet
+# Spotify: spotipy library for spotify API
+# Weather data: Dark Sky API
+# Google sheet: Gspread API
 
+
+### Google drive script ###
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name('Credentials.json', scope)
+gc = gspread.authorize(credentials)
+doc = gc.open_by_url('https://docs.google.com/spreadsheets/d/1tdNvPJiN7f1mJFN0sjtZMy7s_MwcuzsVzBkgFAtgKRk/edit#gid=0')
+
+### Credentials for spotify API ###
 #problem with pip install of spotipy - installs 2.0.1 so pull from github
+Spotifyusername = '1117981227'
+Spotifyscope = 'user-read-private user-read-currently-playing user-read-playback-state user-read-recently-played'
+Spotifyclient_id = 'bcfcac8b1e7748ad855ea59ba5554548'
+Spotifyclient_secret = 'b9f0390792be4596ab54e964863bd134'
+Spotifyredirect_uri = 'http://google.com/'
 
+### Credentials for weather API ###
+darksky_key = "5809cc0ad2768d8c9dcd6ee3bc868dec"
 
-#Enter user name ect
-#Can use a except code
+### Spotify update ###
+#Time stamp, artist name, album name, duration of song, album image url
+# [time, name, artist, album, duration, album_image_url, spotify url, trackid, trackplaying]
+try:
+    spotifydata = Spotify_API(Spotifyusername, Spotifyscope, Spotifyclient_id, Spotifyclient_secret, Spotifyredirect_uri)
+    print(spotifydata[-1])
+    if spotifydata[-1] == False:
+        print('No track playing')
+    elif spotifydata[-1] == True:
+        doc.worksheet('Spotify').append_row(spotifydata)
+        print('Spotify data added')
+except:
+    print('No track playing')
 
-
-your_username = '1117981227'
-scope = 'user-read-private user-read-currently-playing user-read-playback-state user-read-recently-played'
-client_id = 'bcfcac8b1e7748ad855ea59ba5554548'
-client_secret = 'b9f0390792be4596ab54e964863bd134'
-redirect_uri = 'http://google.com/'
-
-token = util.prompt_for_user_token(
-        username=your_username,
-        scope=scope,
-        client_id=client_id,
-        client_secret=client_secret,
-        redirect_uri=redirect_uri)
-
-#Create spotify optject with permissions
-spotifyObject = spotipy.Spotify(auth=token)
-
-# Get current device
-d = spotifyObject.current_user_recently_played()
-devices = spotifyObject.current_user_playing_track()
-
-print(devices['timestamp'])
-print(devices['item']['name'])
-print(devices['item']['album']['artists'][0]['name'])
-print(devices['item']['album']['name'])
-print(devices['item']['duration_ms'])
-print(devices['item']['album']['images'][0])
+### Dark Sky Weather API update ###
+# [time, cloudcover, windspeed, temp, humidity, icon, precip]
+try:
+    weatherdata = dark_sky_API(darksky_key)
+    doc.worksheet('Weather').append_row(weatherdata)
+    print('Weather data added')
+except:
+    print('No weather data')
 
